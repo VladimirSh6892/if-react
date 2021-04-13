c76a9c6e34640a6e2694691bf3e3fc3c763133a6
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-04-13 16:46:56 +0300

    app with guest loves

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {data} from "./constants/array"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="guests">
+      <div className="container">
+        <h2 className="guests-loves__title">Homes guests loves</h2>
+        <div className="guest-loves__slider-wrapper">
+          <div className="guest-loves__swiper-container">
+            <div className="guests-loves__image guest-loves__swiper-wrapper">
+              <figure>
+                <div>
+                  <img
+                    className="guests-loves__image-photo"
+                    src={data.imageUrl}
+                    alt={data.country}
+                  />
+                </div>
+                <figcaption className="guests-loves__destination">
+                  <span className="guests-loves__hotel">{data.name}</span>
+                  <span className="guests-loves__city">{data.city}</span>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
