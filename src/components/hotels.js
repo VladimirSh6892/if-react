9a9956f770998c575b29bc1b70f45f637ff6fbb0
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-04-14 21:37:40 +0300

    replace file and add swiper

---
+++ src/components/hotels.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { data } from "../constants/array";
+import SwiperCore, { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+
+SwiperCore.use([Navigation]);
+
+export default function Hotel() {
+    return  (
+    <Swiper
+        spaceBetween={16}
+        slidesPerView={4}
+        navigation
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+    >
+        {data.map((hotels) =>
+
+            <SwiperSlide key={hotels.id}>
+                <figure className="guests-loves__image-wrapper__swiper-slide">
+                    <div className="guests-loves__image">
+                        <img className="guests-loves__image-photo" src={hotels.imageUrl} alt={hotels.country} />
+                    </div>
+                    <figcaption className="guests-loves__destination">
+                        <span className="guests-loves__hotel">{hotels.name}</span>
+                        <span className="guests-loves__city">{hotels.city}</span>
+                    </figcaption>
+                </figure>
+            </SwiperSlide>   
+        )}
+    </Swiper> )
+};
