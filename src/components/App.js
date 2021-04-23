0ce23554cadfa1f379d79c306f97397377c85b31
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-04-23 15:02:15 +0300

    corected with eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import "./App.css";
-import Hotel from "./Hotel";
+import React from 'react';
+import './App.css';
+import './global.css';
+import './guestLoves.css';
+import Hotel from './Hotel';
 
 function App() {
-  return (
-    <section className="guests">
-      <div className="container">
-        <h2 className="guests-loves__title">Homes guests loves</h2>
-        <div className="guest-loves__slider-wrapper">
-          <div className="guest-loves__swiper-container">
-            <div className="guests-loves__image__swiper-wrapper">
-              <Hotel />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="guests">
+			<div className="container">
+				<h2 className="guests-loves__title">Homes guests loves</h2>
+				<div className="guest-loves__slider-wrapper">
+					<div className="guest-loves__swiper-container">
+						<div className="guests-loves__image__swiper-wrapper">
+							<Hotel />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
