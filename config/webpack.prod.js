f69af07bdbf1c3e8b5b9c16c321f4d4d8719a476
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-04-27 23:25:37 +0300

    corected mistakes

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,72 +3,67 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/components/index.js',
-    mode: 'production',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: "app.js"
-    },
-    module: {
-        rules: [
-            {
-            test: /\.html$/,
-            use: 'html-loader'
-            },
-            {
-            test: /\.css$/,
-            use:
-                [
-                'style-loader',
-                'css-loader'
-                ]
-            },
-            {
-            test: /\.(js|jsx)$/,
-            use: 'babel-loader'
-            },
-            {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-                ]
-            },
-            {
-            test: /\.png$/,
-            use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }]
-            },
-            {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'fonts/',
-                        name: '[name].[ext]'
-                    }
-            }]
-            },
-            {
-            test: /\.txt$/,
-            use: 'raw-loader'
-            }           
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            inject: true,
-            template: './src/components/index.html'
-    }),
-        new CleanWebpackPlugin()
-    ],
-    resolve: {
-        extensions: [
-        '.js',
-        '.jsx'
-        ]
-    }      
+	entry: './src/components/index.js',
+	mode: 'production',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'app.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: 'html-loader',
+			},
+			{
+				test: /\.css$/,
+				use:
+			[
+				'style-loader',
+				'css-loader',
+			],
+			},
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						outputPath: 'img/',
+						name: '[name].[ext]',
+					},
+				}],
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						outputPath: 'fonts/',
+						name: '[name].[ext]',
+					},
+				}],
+			},
+			{
+				test: /\.txt$/,
+				use: 'raw-loader',
+			},
+		],
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			inject: true,
+			template: './src/components/index.html',
+		}),
+		new CleanWebpackPlugin(),
+	],
+	resolve: {
+		extensions: [
+			'.js',
+			'.jsx',
+		],
+	},
 };
