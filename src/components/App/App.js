c6ae2d2af720e5129626aa3d03e687c0df1fe350
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-05-12 20:10:40 +0300

    add HeaderSection

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GuestLoves from '../GuestLoves/GuestLoves';
+import HeaderSection from '../HeaderSection/HeaderSection';
 import './App.css';
 
 function App() {
 	return (
 		<>
+			<HeaderSection />
 			<GuestLoves />
 		</>
 	);
